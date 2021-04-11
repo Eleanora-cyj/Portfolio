0aae9512627d7dc27ef7fba6f3e915317e1294f0
Eleanora-cyj <61038787+Eleanora-cyj@users.noreply.github.com>
2021-04-12 05:40:27 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const cross_icon = document.querySelector('#nav-bar .nav-bar .nav-list .cross');
-const nav_list = document.querySelector('#nav-bar .nav-bar .nav-list');
+const cross_icon = document.querySelector('#nav-bar .nav-list .cross');
+const nav_list = document.querySelector('#nav-bar .nav-list');
 
-cross_icon.addEventListener('click', () => {
+cross_icon.addEventListener("click", function() {
 	nav_list.classList.toggle('open');
 	cross_icon.classList.toggle('active');
+	console.log("Clicking the nav button");
 });
+
 document.body.addEventListener('touchstart', function () { });
